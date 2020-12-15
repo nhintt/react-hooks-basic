407cafd470c520ea9796198a09ded900fe50bcd1
nhintt <nhintt2411@gmail.com>
2020-12-15 22:21:50 +0700

    useState() delete todo in TodoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.scss';
-import ColorBox from './components/ColorBox';
+import {useState} from 'react';
+import TodoList from './components/TodoList';
 
 function App () {
+  const [todoList, setTodoList] = useState ([
+    {id: 1, title: 'Todo 1 '},
+    {id: 2, title: 'Todo 2'},
+    {id: 3, title: 'Todo 3'},
+  ]);
+
+  function handleTodoClick (todo) {
+    // console.log (todo);
+    const index = todoList.findIndex (x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice (index, 1);
+
+    setTodoList (newTodoList);
+  }
+
   return (
     <div className="App">
-      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
